b2db1696bfeae86dbee68114e7f505e1e178207d
sunny <sunny@sunnydeMacBook-Air.local>
2025-03-05 12:18:39 +0800

    測試計數器

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,85 @@
 import DefaultTheme from 'vitepress/theme'
 import Comments from './Comments.vue'
+import ExpandLayout from './layout/expendLayout.vue'
 import { h } from 'vue'
+import { useRoute } from 'vitepress';
+import { onMounted, watch, nextTick } from 'vue'
+
+function reloadBusuanzi() {
+  const busuanziScriptId = 'busuanzi-script';
+
+  // 移除舊的不蒜子
+  const existingScript = document.getElementById(busuanziScriptId);
+  if (existingScript) {
+      existingScript.remove();
+  }
+
+  // 建立新的不蒜子物件
+  const script = document.createElement('script');
+  script.id = busuanziScriptId;
+  script.src = 'https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
+  script.async = true;
+
+  document.body.appendChild(script);
+}
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    // 定義佈局
     return h(DefaultTheme.Layout, null, {
-      'doc-after': () => h(Comments) // 在文件內容後顯示留言區
+      'doc-after': () => h(Comments),
+      "footer-after": () =>
+        h("div", { class: "busuanzi-stats" }, [
+          h("span", { id: "busuanzi_container_site_pv" }, [
+            "網站總訪問量：",
+            h("span", { id: "busuanzi_value_site_pv" }, "載入中..."),
+            " 次",
+          ]),
+          h("span", { style: "margin-left: 15px" }),
+          h("span", { id: "busuanzi_container_page_pv" }, [
+            "本頁訪問量：",
+            h("span", { id: "busuanzi_value_page_pv" }, "載入中..."),
+            " 次",
+          ]),
+          h("span", { style: "margin-left: 15px" }),
+          h("span", { id: "busuanzi_container_site_uv" }, [
+            "網站總訪客數：",
+            h("span", { id: "busuanzi_value_site_uv" }, "載入中..."),
+          ]),
+        ]), // 在文件內容後添加留言區
     })
+  },
+  // 增強應用程序的 setup 函數
+  enhanceApp({ app, router }) {
+    // 如果需要全局註冊組件可以在這裡做
+    app.component('Comments', Comments)
+  },
+  setup() {
+    const route = useRoute()
+    
+    onMounted(async () => {
+      reloadBusuanzi() // 初始化計數器
+    })
+    
+    watch(
+      () => route.path,
+      () => {
+        nextTick(() => {
+          reloadBusuanzi() // 路由變化時重新加載
+        })
+      }
+    )
   }
 }
+
+// export default {
+
+  
+//   extends: DefaultTheme,
+//   Layout: () => {
+//     return h(DefaultTheme.Layout, null, {
+//       'doc-after': () => h(Comments) // 在文件內容後顯示留言區
+//     })
+//   }
+// }
---
+++ docs/.vitepress/theme/layout/expendLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <Layout :class="[frontmatter.class]">
+        <template #doc-before>
+            <div class="info-box">
+                <span class="info">✍️ {{ frontmatter.author || 'Opsehll' }}</span>
+                <div v-if="lastUpdated !== '' || frontmatter.createdAt" class="date-box">
+                    📆
+                    <span v-if="lastUpdated !== ''">Last Updated：{{ lastUpdated }}</span>
+                    <span v-if="frontmatter.createdAt">Created：{{ frontmatter.createdAt }}</span>
+                </div>
+
+                <div class="read">
+                    👀 已被閱讀：
+                    <span id="busuanzi_value_page_pv">Loading</span>
+                    <span id="busuanzi_value_site_pv" class="number">Loading</span>
+                    <span id="busuanzi_value_site_uv" class="number">Loading</span>
+                    次
+                </div>
+
+                <div v-if="frontmatter.tags" class="tag-box">
+                    🏷️
+                    <a v-for="tag in frontmatter.tags" :key="tag" class="tag-info"
+                        :href="`/tags-list.html?tag=${tag}&page=1`">{{ tag }}</a>
+                </div>
+            </div>
+        </template>
+
+        <template #aside-ads-before>
+            <div class="busuanzi-box">
+                Opshell 的 Blog
+                <div class="busuanzi">
+                    已有： <span id="busuanzi_value_site_pv" class="number">Loading</span> 次觀看
+                </div>
+                <div class="busuanzi">
+                    已有： <span id="busuanzi_value_site_uv" class="number">Loading</span> 個人來過
+                </div>
+            </div>
+        </template>
+    </Layout>
+</template>
