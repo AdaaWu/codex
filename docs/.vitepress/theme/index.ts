ed79f42493a1bc292bf32ad4287d6f4057d8e55e
Sunny.cat <idonga500@gmail.com>
2025-03-06 19:09:44 +0800

    上了一些草稿

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,18 +10,18 @@ export default {
     
     return h(DefaultTheme.Layout, null, {
       'doc-after': () => [h(Comments), h('div', { class: 'page-stats' }, [
-        '本頁瀏覽量：',
-        h('span', { id: 'busuanzi_value_page_pv' }),
+        ' 總瀏覽量：',
+        h('span', { id: 'busuanzi_value_site_pv' }),
       ]),
       ],
       // 在文件內容後顯示留言區
-      'doc-bottom': () =>
-        h('div', { class: 'busuanzi-stats' }, [
-          '總訪客數：',
-          h('span', { id: 'busuanzi_value_site_uv' }),
-          ' | 總瀏覽量：',
-          h('span', { id: 'busuanzi_value_site_pv' }),
-        ]),
+      // 'doc-bottom': () =>
+      //   h('div', { class: 'busuanzi-stats' }, [
+      //     '總訪客數：',
+      //     h('span', { id: 'busuanzi_value_site_uv' }),
+      //     ' | 總瀏覽量：',
+      //     h('span', { id: 'busuanzi_value_site_pv' }),
+      //   ]),
     })
   }
 } satisfies Theme
